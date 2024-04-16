65e6a0872f7b5f1cdbb42e2fc4d4105d40499330
Olakunle Raheem <olaks.raheem@gmail.com>
2024-04-16 18:37:11 +0100

    Unlinked railway backend

--- src/pages/RiskTolerance.tsx
+++ src/pages/RiskTolerance.tsx
@@ -1,40 +1,37 @@
 import { useEffect, useState } from "react";
 import SliderComp from "../components/ui/Slider";
 import "rc-slider/assets/index.css";
-import { stockDataType } from "../libs/fakeData";
-import { getPortfolio } from "../libs/apis";
-
-
+import { stockDataType, stockData } from "../libs/fakeData";
 
 export default function RiskTolerance() {
   const [slideValue, setSlideValue] = useState<number[]>([0]);
   const [currentStock, setCurrentStock] = useState<stockDataType | null>(
-    null
+    stockData[slideValue[0]]
   );
-  const handleSlideSelection = async (value: number) => {
-    const stock = await getPortfolio(value.toString());
-    setCurrentStock(stock);
-  };
 
-  console.log(currentStock);
+  console.log(slideValue[0]);
+  console.log(currentStock)
+
   useEffect(() => {
-     if(!currentStock){
-       handleSlideSelection(0);
-     }
-  }, [currentStock])
+    setCurrentStock(stockData[slideValue[0]]);
+  }, [slideValue]);
+
   return (
     <div className="flex pl-10 py-10 flex-col mt-10 gap-10 min-h-screen">
       <p className="text-xl font-bold">Please check your risk tolerance</p>
-      <p>Tolerance or Risk Score: <span className="text-lg font-semibold text-purple-800">{slideValue[0]}</span></p>
-      <SliderComp
-        slideValue={slideValue}
-        setSlideValue={setSlideValue}
-        handleSlideSelection={handleSlideSelection}
-      />
+      <p>
+        Tolerance or Risk Score:{" "}
+        <span className="text-lg font-semibold text-purple-800">
+          {slideValue[0]}
+        </span>
+      </p>
+      <SliderComp slideValue={slideValue} setSlideValue={setSlideValue} />
 
       {/* chart */}
 
       <div className="flex flex-col gap-2">
+        
+        
         {currentStock &&
           Object.keys(currentStock).map((stock: string) => {
             if (
